297673d8760adae5a5ef8b87ab55d657112e218e
horaciod12 <daddarioh@gmail.com>
2017-05-27 14:50:40 -0300

    new styles

--- module_phone/js/app.js
+++ module_phone/js/app.js
@@ -1,20 +1,14 @@
 var myPhone = (function() {
 
-var value;
-
 
 $('button').on('click', function(){
 	
 	var value = $(this).val();
-	
-	console.log(value);
 
 	$('#display').append(value);
 
 
-})
-
-
+});
 
 
 
