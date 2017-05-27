6cbb13458392e680e4ab6fc9a7c8789ed7ff8dc4
horaciod12 <daddarioh@gmail.com>
2017-05-27 12:51:58 -0300

    function added

--- module_phone/js/app.js
+++ module_phone/js/app.js
@@ -1,5 +1,19 @@
 var myPhone = (function() {
 
+var value;
+
+
+$('button').on('click', function(){
+	
+	var value = $(this).val();
+	
+	console.log(value);
+
+	$('#display').append(value);
+
+
+})
+
 
 
 
